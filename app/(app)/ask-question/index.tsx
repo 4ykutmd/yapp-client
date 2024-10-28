95eb47853c27c491a1a6b1999e370eb70bbc1ae3
Emir Ufak <emir.ufak@gmail.com>
2024-10-29 00:05:20 +0300

    button, input

--- app/(app)/ask-question/index.tsx
+++ app/(app)/ask-question/index.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet } from 'react-native';
+import { Button, Image, Pressable, StyleSheet, TextInput } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import { Text, View } from '@/components/Themed';
+import { useState } from 'react';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 export default function Page() {
+  const [input, setInput] = useState('');
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    //TODO mesaj kutusu, foto yukleme
     <View style={styles.container}>
-      <Text>
-        Soru sor
-      </Text>
+        {image && <Image source={{ uri: image }} style={styles.image} />}
+
+      <View style={{flexDirection:'row', gap:5}}>
+
+        <Pressable onPress={pickImage} style={{width:50, height:50, backgroundColor:'#5781ea', borderRadius:20, justifyContent:'center', alignItems:'center'}}>
+          <AntDesign name="picture" size={24} color="white" />
+        </Pressable>
+
+        <TextInput
+          style={styles.input} placeholder='Soru sor' placeholderTextColor={'gray'} 
+          onChangeText={(e) => {setInput(e)}}
+        />
+
+        <Pressable style={{width:50, height:50, backgroundColor:'#5781ea', borderRadius:20, justifyContent:'center', alignItems:'center'}}>
+          <Ionicons name="send" size={24} color="white" />
+        </Pressable>
+      </View>
 
     </View>
   );
@@ -29,4 +63,20 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  image: {
+    width: 100,
+    height: 100,
+    margin:10,
+    borderWidth:1,
+    borderColor: '#5781ea'
+  },
+  input: {
+    width:'65%', 
+    height:50, 
+    borderRadius:30, 
+    padding:10, 
+    paddingLeft:15, 
+    borderWidth:2, 
+    borderColor:'#5781ea',
+  }
 });
